2fcb91fcaefaceb3dd52dfce2fbc2079c04de5e6
Michael Hildebrand-Faust <mhfaust@mail.com>
2019-01-05 09:15:29 -0800

    fixed board transformed prematurely

--- src/boards/empty.js
+++ src/boards/empty.js
@@ -1,14 +1,14 @@
-"use strict";
-exports.__esModule = true;
-var pieces_1 = require("constants/pieces");
-exports["default"] = (function () { return [
-    /*         1  2  3  4  5  6  7  8  */
-    /*  A  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  B  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  C  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  D  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  E  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  F  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  G  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-    /*  H  */ [pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__, pieces_1.__],
-]; });
+import { BK,BQ,BR,BN,BB,BP,WK,WQ,WR,WN,WB,WP,EMPTY,__ } from 'constants/pieces'
+import { A1,A2,A3,A4,A5,A6,A7,A8,B1,B2,B3,B4,B5,B6,B7,B8,C1,C2,C3,C4,C5,C6,C7,C8,D1,D2,D3,D4,D5,D6,D7,D8,E1,E2,E3,E4,E5,E6,E7,E8,F1,F2,F3,F4,F5,F6,F7,F8,G1,G2,G3,G4,G5,G6,G7,G8,H1,H2,H3,H4,H5,H6,H7,H8 } from '../constants/algebraic'
+
+export default () => [
+/*         1  2  3  4  5  6  7  8  */
+/*  A  */ [__,__,__,__,__,__,__,__],
+/*  B  */ [__,__,__,__,__,__,__,__],
+/*  C  */ [__,__,__,__,__,__,__,__],
+/*  D  */ [__,__,__,__,__,__,__,__],
+/*  E  */ [__,__,__,__,__,__,__,__],
+/*  F  */ [__,__,__,__,__,__,__,__],
+/*  G  */ [__,__,__,__,__,__,__,__],
+/*  H  */ [__,__,__,__,__,__,__,__],
+];
