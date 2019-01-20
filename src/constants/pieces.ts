94754e1a250d689af2a38eeea02ce347846a384c
Michael Hildebrand-Faust <mhfaust@mail.com>
2019-01-20 10:44:08 -0800

    dont assume var is destructured

--- src/constants/pieces.ts
+++ src/constants/pieces.ts
@@ -37,25 +37,25 @@ export enum Piece {
 const { BK,BQ,BR,BN,BB,BP,WK,WQ,WR,WN,WB,WP,Empty,__ } = Piece;
 
 
-const blackPieces : Array<string> = [BK,BQ,BR,BN,BB,BP];
-const whitePieces  : Array<string> = [WK,WQ,WR,WN,WB,WP];
+const blackPieces : Array<string> = [Piece.BK,Piece.BQ,Piece.BR,Piece.BN,Piece.BB,Piece.BP];
+const whitePieces  : Array<string> = [Piece.WK,Piece.WQ,Piece.WR,Piece.WN,Piece.WB,Piece.WP];
 
 export const BLACK_PIECES = Object.freeze(new Set(blackPieces));
 export const WHITE_PIECES = Object.freeze(new Set(whitePieces));
 export const ALL_PIECES = Object.freeze(new Set([...blackPieces, ...whitePieces]));
 
 export const unicodeSymbols = {
-    [WK]: '♔',
-    [WQ]: '♕',
-    [WR]: '♖',
-    [WB]: '♗',
-    [WN]: '♘',
-    [WP]: '♙',
-    [BK]: '♚',
-    [BQ]: '♛',
-    [BR]: '♜',
-    [BB]: '♝',
-    [BN]: '♞',
-    [BP]: '♟',
-    [__]: ' '
+    [Piece.WK]: '♔',
+    [Piece.WQ]: '♕',
+    [Piece.WR]: '♖',
+    [Piece.WB]: '♗',
+    [Piece.WN]: '♘',
+    [Piece.WP]: '♙',
+    [Piece.BK]: '♚',
+    [Piece.BQ]: '♛',
+    [Piece.BR]: '♜',
+    [Piece.BB]: '♝',
+    [Piece.BN]: '♞',
+    [Piece.BP]: '♟',
+    [Piece.__]: ' '
 }
